f7cee5dca26cea700b529e387f9887d8d2a85c71
USUARIO15-PC\Usuario01 <katty.montoya24@gmail.com>
2017-01-11 13:41:04 -0500

    Avance del grupo

---
+++ PROYECTO11.01.2017.py
@@ -0,0 +1,27 @@
+from tkinter import *
+tk = Tk()
+canvas = Canvas(tk, width=730, height=700)
+canvas.pack()
+
+imagen=PhotoImage(file="esenario.png")
+my_image=PhotoImage(file="bom.png")
+
+
+canvas.create_image(0,0,anchor=NW,image=imagen)
+canvas.create_image(60,85,anchor=NW,image=my_image)
+
+def movetriangle(event):
+    if event.keysym == 'Up':
+        canvas.move(2, 0, -3)
+    elif event.keysym == 'Down':
+        canvas.move(2, 0, 3)
+    elif event.keysym == 'Left':
+        canvas.move(2, -3, 0)
+    else:
+        canvas.move(2, 3, 0)
+canvas.bind_all('<KeyPress-Up>', movetriangle)
+canvas.bind_all('<KeyPress-Down>', movetriangle)
+canvas.bind_all('<KeyPress-Left>', movetriangle)
+canvas.bind_all('<KeyPress-Right>', movetriangle)
+tk.mainloop()
+
